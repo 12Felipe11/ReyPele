9d8676bcf7ba242cf42a0d8f8356b75c79dfa0c5
Yohan-Diaz <dev3@coondev.com>
2026-04-19 22:37:04 -0500

    DEL Avance

--- Avance/src/dominio/actuator/AlarmActuator.java
+++
@@ -1,8 +0,0 @@
-package dominio.actuator;
-
-/** Actuador de alarma (HU-06, HU-07). */
-public class AlarmActuator extends Actuator {
-    public AlarmActuator(String id, String zone) {
-        super(id, zone);
-    }
-}
